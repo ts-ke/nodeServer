623989cfd233417d7c8161b0aff87f868cee324d
Kenneth <kennethtsang9286@gmail.com>
2019-08-30 14:38:19 +1000

    Fix typo

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ const TEST_URL = 'https://localhost';
 const TOKEN =
 	'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NjI1NzI0NjQsImV4cCI6MTU5NDEwODQ2OSwiYXVkIjoid3d3LnN0dWRlbnRzLjJoYXRzLmNvbS5hdSIsInN1YiI6InVzZXJAMmhhdHMuY29tLmF1IiwiR2l2ZW5OYW1lIjoiSm9obiIsIlN1cm5hbWUiOiJTbm93IiwiRW1haWwiOiJqb2huc25vd0AyaGF0cy5jb20uYXUiLCJSb2xlIjoiSmFuaXRvciJ9.BEEqb2ihfP0ec8TBu91T9lk0kcBKpz1NkJv4PpyjxdE';
 
+const NODE_ENV = 'local'; // "local" || "development"
+
 exports.TEST_URL = TEST_URL;
 exports.PORT = PORT;
 exports.TOKEN = TOKEN;
-
-const NODE_ENV = 'local'; // "local" || "development"
+exports.NODE_ENV = NODE_ENV;
